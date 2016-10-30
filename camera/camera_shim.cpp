6199b443136a92365281b347f76aa0b43724245a
JishnuSur <jishnusur16@gmail.com>
2016-10-30 14:15:23 +0530

    Update camera_shim.cpp

--- camera/camera_shim.cpp
+++ camera/camera_shim.cpp
@@ -15,3 +15,13 @@
  */
 
 const char *_ZN7android18gClientPackageNameE;
+const char *_ZN7android16CameraParameters31KEY_SUPPORTED_VIDEO_SIZES_60FPSE = "support-60fps-video-sizes";
+
+extern "C" {
+
+extern ssize_t _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void*, void*);
+
+ssize_t _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list) {
+    return _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(thiz, list);
+}
+
